7ba0a1c1bf9de1642e3d5d2b8c8bbd4733bb596d
StudentTechsima <156827118+StudentTechsima@users.noreply.github.com>
2024-12-18 20:44:50 +0530

    Update Registration.jsx

--- frontend/hotelbooking/src/Components/Registration.jsx
+++ frontend/hotelbooking/src/Components/Registration.jsx
@@ -38,7 +38,7 @@ const Registration = () => {
         formData.append('password', password);
 
 
-        const res = await axios.post(`http://localhost:3000/client/register`, formData);
+        const res = await axios.post(`https://hoteltechsima-backend.onrender.com/client/register`, formData);
 
         if (res.status === 201) {
           toast.success('User Register Successfully');
